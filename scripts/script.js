f636055a97353b98bafbbee52a239e6e7438a6d5
Denis Suvorovsky <magicianprofound@gmail.com>
2022-07-03 12:51:10 +0300

    hotfix

--- scripts/script.js
+++ scripts/script.js
@@ -7,17 +7,20 @@ const UserName = document.querySelector('.profile__name');
 const UserJob = document.querySelector('.profile__job');
 const FormElement = document.querySelector('.form')
 
+
+//открытие поп-апа и перенос данных из разметки в форму
 EditBtn.addEventListener('click', function() {
     Popup.classList.add('popup_opened')
     NameInput.value = UserName.textContent
     JobInput.value = UserJob.textContent
 })
 
-
+//закрытие поп-апа
 PopupClose.addEventListener('click', function () {
     Popup.classList.remove('popup_opened')
 });
 
+//изменение и сохранение данных из поп-апа
 function FormSubmitHandler(evt) {
     evt.preventDefault();
     UserName.textContent = NameInput.value
