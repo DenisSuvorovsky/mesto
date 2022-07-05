b8ab3e25ff52c4b8e1a78af26b68565cf46459da
Denis Suvorovsky <magicianprofound@gmail.com>
2022-07-05 16:07:03 +0300

    hotfix: index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,31 @@
 const popupClose = document.querySelector('.popup__close');
 const editBtn = document.querySelector('.profile__edit-btn');
 const popup = document.querySelector('.popup');
-const jobInput = document.querySelector('.popup__input_job');
-const nameInput = document.querySelector('.popup__input_name');
+const jobInput = document.querySelector('.popup__input_type_input-job');
+const nameInput = document.querySelector('.popup__input_type_input-name');
 const userName = document.querySelector('.profile__name');
 const userJob = document.querySelector('.profile__job');
-const formElement = document.querySelector('.form')
+const formElement = document.querySelector('.popup__form');
 
 
 //открытие поп-апа и перенос данных из разметки в форму
 editBtn.addEventListener('click', function() {
-    popup.classList.add('popup_opened')
-    nameInput.value = userName.textContent
-    jobInput.value = userJob.textContent
+    popup.classList.add('popup_opened');
+    nameInput.value = userName.textContent;
+    jobInput.value = userJob.textContent;
 })
 
 //закрытие поп-апа
 popupClose.addEventListener('click', function () {
     popup.classList.remove('popup_opened')
-});
+})
 
 //изменение и сохранение данных из поп-апа
-function FormSubmitHandler(evt) {
-    evt.preventDefault();
+function formSubmitHandler(evt) {
+    evt.preventDefault()
     userName.textContent = nameInput.value
     userJob.textContent = jobInput.value
     popup.classList.remove('popup_opened')
 }
-formElement.addEventListener('submit', FormSubmitHandler);
+
+formElement.addEventListener('submit', formSubmitHandler);
