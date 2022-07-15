d33423670db38304c1f525927881756a5b50fa99
Denis Suvorovsky <magicianprofound@gmail.com>
2022-07-15 16:32:27 +0300

    feature: pop-up close & render cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,17 @@
 const popupClose = document.querySelector('.popup__close');
 const editProfileBtn = document.querySelector('.profile__edit-btn');
 const addProfileBtn = document.querySelector('.profile__add-btn');
-const popup = document.querySelector('.popup');
+const editProfilePopup = document.querySelector('.popup_type_edit');
 const jobInput = document.querySelector('.popup__input_type_input-job');
 const nameInput = document.querySelector('.popup__input_type_input-name');
 const userName = document.querySelector('.profile__name');
 const userJob = document.querySelector('.profile__job');
 const formElement = document.querySelector('.popup__form');
+const addNewCardPopup = document.querySelector('.popup_type_new-card');
+const cardName = addNewCardPopup.querySelector('.popup__input_type_card-name');
+const cardSrc = addNewCardPopup.querySelector('.popup__input_type_card-src');
+const addCardForm = addNewCardPopup.querySelector('.popup__form');
+const popupList = document.querySelectorAll('.popup');
 const initialCards = [
     {
       name: 'Архыз',
@@ -34,17 +39,16 @@ const initialCards = [
     }
   ];
 
-
  //открытие поп-апа и перенос данных из разметки в форму
  editProfileBtn.addEventListener('click', () => {
-  popup.classList.add('popup_opened');
+  editProfilePopup.classList.add('popup_opened');
   nameInput.value = userName.textContent;
   jobInput.value = userJob.textContent;
 })
 
 
 //закрытие поп-апа
-popupClose.addEventListener('click', () => {popup.classList.remove('popup_opened');})
+popupClose.addEventListener('click', () => {editProfilePopup.classList.remove('popup_opened');})
 
 
 //изменение и сохранение данных из поп-апа
@@ -52,13 +56,13 @@ function formSubmitHandler(evt) {
     evt.preventDefault()
     userName.textContent = nameInput.value
     userJob.textContent = jobInput.value
-    popup.classList.remove('popup_opened')
+    editProfilePopup.classList.remove('popup_opened')
 }
 formElement.addEventListener('submit', formSubmitHandler);
 
 
 //Вывод шаблона карточки на экран
-const addCard =  (item) => {
+const renderCards =  (item) => {
   const cardTemplate = document.querySelector('.template').content;
   const cardClone = cardTemplate.querySelector('.elements__card').cloneNode('true');
   const elementsList = document.querySelector('.elements');
@@ -69,5 +73,34 @@ const addCard =  (item) => {
 }
 
 initialCards.forEach(function(item) {
-  addCard(item);
+  renderCards(item);
 });
+
+  
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+}
+
+popupList.forEach((popup) =>  {
+  popup.addEventListener('click', (evt) => {
+    if (evt.target.classList.contains('popup__close')) {
+      closePopup(popup);
+    }
+  })
+})
+
+addProfileBtn.addEventListener('click', () => {
+addNewCardPopup.classList.add('popup_opened');
+});
+
+addCardForm.addEventListener('submit',function (evt) {
+evt.preventDefault();
+addNewCardPopup.classList.remove('popup_opened');
+});
+
+function createNewCard(name, link) {
+  const template = document.querySelector('.template').content;
+  const newElement = template.querySelector('.elements__card').cloneNode('true');
+  const elementsList = document.querySelector('.elements');
+  
+}
