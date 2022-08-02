0a71f17147e0d856d1e68bc5ca723d7bb64812f2
Denis Suvorovsky <magicianprofound@gmail.com>
2022-08-02 19:21:12 +0300

    hotfix: initial cards add

--- scripts/index.js
+++ scripts/index.js
@@ -16,33 +16,6 @@ const elementsList = document.querySelector('.elements');
 const imagePopup = document.querySelector('.popup_type_image');
 const image = imagePopup.querySelector('.popup__img');
 const caption = imagePopup.querySelector('.popup__caption');
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
 
 
 function handleDeleteClick(evt) {
