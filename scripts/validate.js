a8404e9b13cb1a2af6ebcd635e89a8ae449ffccc
Denis Suvorovsky <magicianprofound@gmail.com>
2022-10-01 02:08:13 +0300

    сделать стили ошибок

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,34 +1,33 @@
 enableValidation({
     formSelector: '.popup__form',
     inputSelector: '.popup__input',
-    submitButtonSelector: '.popup__button',
-    inactiveButtonClass: 'popup__button_disabled',
+    submitButtonSelector: '.popup__submit',
+    inactiveButtonClass: 'popup__submit_disabled',
     inputErrorClass: 'popup__input-error',
     errorClass: 'popup__input-error_active'
 });
 
+const showInputError = (element) => {
+    element.classList.add('form__input_type_error');
+    // Показываем сообщение об ошибке
+    formError.classList.add('form__input-error_active');
+};
 
-
-function showError(form, input, config) {
-    const error = form.querySelector(`#${input.id}-error`);
-    input.classList.add(config.inputErrorClass);
-    error.textContent = input.validationMessage;
-}
 
 function hideError(form, input, config) {
-    const error = form.querySelector(`#${input.id}-error`);
+    const error = form.querySelector(`.${input.id}-error`);
     input.classList.remove(config.inputErrorClass);
     error.classList.remove(config.errorClass);
     error.textContent = '';
 }
 
 function validateInput(input, form, config) {
-    const error = form.querySelector(`#${input.id}-error`);
+    const error = form.querySelector(`.${input.id}-error`);
     if (!input.validity.valid) {
         showError(form, input, config);
         error.classList.add(config.errorClass);
     } else {
-       hideError(form, input, config);
+        hideError(form, input, config);
     }
 }
 
@@ -45,12 +44,9 @@ function enableValidation(config) {
     const form = document.querySelector(config.formSelector);
     const forms = Array.from(document.querySelectorAll(config.formSelector));
     forms.forEach((form) => {
-        form.addEventListener('submit', (evt) => {
-            evt.preventDefault();
             setHandlers(form, config);
         })
-    })
-      setHandlers(form, config);
+
   }
 
 
